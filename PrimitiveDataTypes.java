e090d6c999cb3a7d941e7b3cc9e66c06aff6ae16
TesterDR <93728750+TesterDR@users.noreply.github.com>
2022-06-13 15:58:55 +0500

    Add files via upload

---
+++ PrimitiveDataTypes.java
@@ -0,0 +1,57 @@
+package Lesson2;
+
+public class PrimitiveDataTypes {
+public static void main (String [] args) {
+
+    byte b1 = 10;
+    byte b2 = 20;
+    byte b3 = 100;
+
+    short s1 = 5;
+    short s2 = -10;
+    short s3 = - 0;
+
+    int i1 = 500;
+    int i2 = 500;
+    int i3 = 500 + 500;
+
+    long l1 = 1000;
+    long l2 = 1000000000000000009L;
+
+    float f1 = 3.146666666666F;
+    float f2 = 200000000000000000888888888888888F;
+
+    double d1 = 5.50000000000000000000000000000000000044444444444443333333333;
+    double d2 = -47777.999947373000000077;
+
+    char c1 = 'h'; // comment 1
+    char c2 = '1';
+    char c3 = '-';
+    char c4 = ' ';
+    char c5 = 1;
+
+    System.out.println (b1 + b2); // comment 2
+    System.out.println (c5);
+
+    boolean bool1 = true;
+    boolean bool2 = false;
+
+    System.out.println(bool1);
+
+    int a1 = 60;
+    int a2 = 0b111101;
+
+    int a3 = 074;
+
+    int a4 = 1_000_000____0;
+
+    System.out.println(a2);
+
+
+
+
+
+
+    }
+};
+
---
+++ domaschkapovtor.java
@@ -0,0 +1,74 @@
+package Lesson2;
+
+public class domaschkapovtor {
+    public static void main (String [] args) {
+
+        byte b2 = 0b1100;
+        byte b8 = 014;
+        byte b10 = 12;
+        byte b16 = 0xC;
+
+        System.out.println("");
+        System.out.println(b2);
+        System.out.println(b8);
+        System.out.println(b10);
+        System.out.println(b16);
+        System.out.println("");
+
+        short s2 = 0b101_000_101_00;
+        short s8 = 0_2424;
+        short s10 = 1300;
+        short s16 = 0x514;
+
+        System.out.println(s2);
+        System.out.println(s8);
+        System.out.println(s10);
+        System.out.println(s16);
+        System.out.println("");
+
+        int i2 = 0b0;
+        int i8 = 0_0;
+        int i10 = 0;
+        int i16 = 0x0;
+
+        System.out.println(i2);
+        System.out.println(i8);
+        System.out.println(i10);
+        System.out.println(i16);
+        System.out.println("");
+
+        long l2 = 0b1010L;
+        long l8 = 0_12L;
+        long l10 = 10L;
+        long l16 = 0xAL;
+
+        System.out.println(l2);
+        System.out.println(l8);
+        System.out.println(l10);
+        System.out.println(l16);
+        System.out.println("");
+
+        float f1 = 3.14f;
+        double d1 = 3.15;
+        boolean bool1 = true;
+        boolean bool2 = false;
+
+        System.out.println(f1);
+        System.out.println(d1);
+        System.out.println (bool1);
+        System.out.println(bool2);
+
+        char c1 = 'A';
+        char c2 = 4626;
+        char c3 = '\u1212';
+
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println (c3);
+
+
+
+
+
+    }
+}
